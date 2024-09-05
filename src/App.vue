4e3d89ba626910b9e1073740bfd2ec59d82481d1
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2024-09-05 21:17:16 +0200

    authState update Log in/Log out button

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
-
     <nav>
-      <router-link to="/login">Connexion</router-link>
-      <router-link to="/register">Inscription</router-link>
-      <router-link to="/users">Utilisateurs</router-link> <!-- Add a link to the user list -->
+        <router-link to="/register">Inscription</router-link>
+        <router-link to="/users">Utilisateurs</router-link> <!-- Add a link to the user list -->
+      <div class="login-logout">
+        <!-- Show "Log in" if the user is not logged in -->
+        <router-link v-if="!authState.isLoggedIn" to="/login" class="login">Connexion</router-link>
+        <!-- Show "Log out" if the user is logged in -->
+        <button v-if="authState.isLoggedIn" @click="logout" class="logout">Se déconnecter</button>
+      </div>
     </nav>
     <router-view />
   </div>
 </template>
 
 <script>
+import { authState } from './auth'; // Import the reactive login state
+
 export default {
   name: 'App',
+  computed: {
+    // Check if the user is logged in by looking for the token in localStorage
+    authState() {
+      return authState; // Use the reactive login state
+    }
+  },
+  methods: {
+    logout() {
+      // Remove the token from localStorage to log the user out
+      localStorage.removeItem('token');
+      authState.isLoggedIn = false; // Update the reactive state
+      this.$router.push('/login'); // Redirect to the login page
+    }
+  }
 };
+
+
 </script>
 
 <style>
@@ -47,7 +69,7 @@ nav {
       transition: color 0.3s ease-in-out;
     }
 
-    &.router-link-active {
+    &.router-link-active:not(.login) {
       font-weight: bolder;
       color: cadetblue;
       font-size: 1.2rem;
@@ -56,6 +78,32 @@ nav {
         border-color: transparent;
       }
     }
+  }
+
+  .login-logout {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+  }
+}
+
+a.login {
+  padding: 10px 15px;
+  font-size: 1rem;
+  background-color: lightslategray;
+  color: white;
+  transition: all 0.3s ease-in-out;
+  &:not(.router-link-active ):hover {
+    color: white;
+    background-color: cadetblue;
+  }
+
+  &.router-link-active {
+    background-color: cadetblue;
+    &:hover {
+    }
+    cursor: default;
+    color: white;
   }
 }
 
@@ -69,6 +117,11 @@ button {
   transition: background-color 0.3s ease-in-out;
   &:hover {
     background-color: cadetblue;
+  }
+
+  &.logout {
+    padding: 10px 15px;
+    font-size: 1rem;
   }
 }
 
